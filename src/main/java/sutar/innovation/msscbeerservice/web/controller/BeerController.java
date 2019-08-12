b859eaa16954fceda33255d40a10553f1e8864b3
Sandeep Sutar <sandeepsutarora@gmail.com>
2019-08-12 20:12:12 +0530

    improvement

---
+++ src/main/java/sutar/innovation/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,35 @@
+package sutar.innovation.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import sutar.innovation.msscbeerservice.web.model.BeerDTO;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId)
+    {
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+   @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beerDTO)
+   {
+    return new ResponseEntity(HttpStatus.CREATED);
+   }
+
+   @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerBuId(@PathVariable({"/{beerId}"}) UUID beerId, @RequestBody BeerDTO beerDTO )
+   {
+       //todo Implementations.
+       return new ResponseEntity(HttpStatus.NO_CONTENT);
+
+   }
+
+
+}
