dc7ebcabbcd3af6282164d9aedc3f2e27833f22f
Sandeep Sutar <sandeepsutarora@gmail.com>
2019-08-13 15:13:08 +0530

    setup persistance

---
+++ src/main/java/sutar/innovation/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,30 @@
+package sutar.innovation.msscbeerservice.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import sutar.innovation.msscbeerservice.domain.Beer;
+import sutar.innovation.msscbeerservice.repositories.BeerRepository;
+
+import java.math.BigDecimal;
+
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects(){
+                Beer beer = Beer.builder().beerName("KingFisher")
+                        .beerStyle("Cold")
+                        .minOnHand(12)
+                        .price(new BigDecimal(3322))
+                        .upc(233333L)
+                        .build();
+    }
+}
---
+++ src/main/java/sutar/innovation/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package sutar.innovation.msscbeerservice.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import sutar.innovation.msscbeerservice.domain.Beer;
+
+import java.util.UUID;
+
+public class BeerRepository //extends PagingAndSortingRepository<Beer, UUID>
+{
+
+}
